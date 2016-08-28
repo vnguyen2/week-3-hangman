22df9608eb9b0dcbdf21bfce9a5d0301e6b8b479
Vincent Nguyen <vnguyen2@gmail.com>
2016-08-27 19:59:09 -0500

    code optimization for cleaner look and function

--- assets/scripts/hangmangame.js
+++ assets/scripts/hangmangame.js
@@ -1,4 +1,5 @@
 
+//variables needed for hangman
 var words = ["pizza","ramen","spaghetti","meatball","ribs","sausage","tacos","burger","chicken","porkchop","hotdog","bacon","salmon","banana"];
 var computerWord = words[Math.floor(Math.random() * words.length)];
 
@@ -10,21 +11,13 @@ var dashes=[],
 	userGuesses=[],
 	currentWord =[];
 
-console.log("Computer chose: " + computerWord);
-
-window.onload = function() {
-	for (var i = 0; i < computerWord.length; i++) {
-			dashes.push("_");
-		}
-		currentWord = dashes.join(" ");
-		document.getElementById("answer").innerHTML = currentWord;
-}
-
+//reset function to start and reset game. Returns all variables to original state
+//and produces new word in dashes
 function gameReset(){
 	computerWord = words[Math.floor(Math.random() * words.length)];
 	dashes=[];
 	userGuesses = [];
-	guesses = 15;
+	guesses = 12;
 
 	for (var i = 0; i < computerWord.length; i++) {
 		dashes.push("_");
@@ -34,14 +27,20 @@ function gameReset(){
 	console.log("New word: " + computerWord);
 }
 
+gameReset();
+
+//function performed when a key is pressed
 document.onkeyup = function(event){
 
 	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 
+	//messages to user during gameplay
 	document.querySelector('.winner').innerHTML = ('');
 	document.querySelector('.loser').innerHTML = ('');
 	document.querySelector('.newLetter').innerHTML = ('');
 
+	//looking to see if user input matches in letters in the chosen word and replacing
+	//the letter in the corresponding dash
 	for (var n = 0; n < computerWord.length; n++) {
 		if (computerWord[n] === userInput) {
 			dashes[n] = userInput;
@@ -50,6 +49,11 @@ document.onkeyup = function(event){
 		}
 	}
 
+	//function to check if letter pressed is an actual letter 
+	//check if letter was already pressed
+	//check if user wins if the computer word and the guessed word match
+	//check if user has ran out of guesses
+	//decrease guesses if user guess is incorrect and push userGuesses into an array
 	function gamePlay(value) {
 	var letterCheck  = /^[a-z]+$/;
 		if (letterCheck.test(value) == false) {
@@ -62,16 +66,15 @@ document.onkeyup = function(event){
 	          	else if(computerWord == currentWord) {
 	          		wins++;
 	          		document.querySelector('.win').play()
-	          		document.querySelector('.winner').innerHTML = ("YOU WIN. NOM NOM NOM");
+	          		document.querySelector('.winner').innerHTML = ("YOU WIN. NOM NOM NOM on " + computerWord);
 		        	gameReset();
 		        	console.log("you win");
 		        } 
 		        else if (guesses == 1) {
 		        	losses++;
 		        	document.querySelector('.lose').play()
-		        	document.querySelector('.loser').innerHTML = ("No food for you LOSER");
+		        	document.querySelector('.loser').innerHTML = ("You don't get to eat any " + computerWord);
 		            gameReset();
-		            console.log("Computer picks new word: " + computerWord);
 		        } 
 		  		else if (computerWord.indexOf(userInput) == -1){
 					guesses--;
@@ -83,6 +86,7 @@ document.onkeyup = function(event){
 	}
     gamePlay(userInput);
 
+    //update html with stats for game as user plays
 	var html = 	"<h1>The Hangry Game</h1>" +
 				"<p>Press any key to start!</p>" +
 				"<p>Wins: " + wins + "</p>" +
@@ -92,3 +96,4 @@ document.onkeyup = function(event){
 
 	document.querySelector(".game").innerHTML = html;
 }
+
